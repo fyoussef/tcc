0c6ae8d821e949279c4761497c82153f9d737b10
Filipi Youssef <fillipyuou@gmail.com>
2024-06-08 16:08:35 +0000

    serve text files

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 import http from 'node:http';
-import process from 'node:process';
 import path from 'node:path';
-import fs from 'node:fs'
+import fs, { createReadStream, readFileSync } from 'node:fs'
 
-const server = http.createServer((req, res) => {
+const sendFile = (res, filePath, contentType) => {
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            const fileNotFound = err.code === 'ENOENT';
+            if (fileNotFound) {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                return res.end('<h1>404 Not Found</h1>');
+            }
+            res.writeHead(500, { 'Content-Type': 'text/html' });
+            return res.end(`<h1>500 Server Error</h1><p>${err}</p>`);
+        } 
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(content, 'utf-8');
+    });
+}
+
+const handleServeFiles = (req, res) => {
     let filePath = './public' + req.url;
     if (req.url === '/') {
-        filePath = './public/index.html'; // Página inicial
+        filePath = './public/index.html';
     }
-    
     const extname = path.extname(filePath);
     let contentType = 'text/html';
-
     switch (extname) {
         case '.js':
             contentType = 'text/javascript';
@@ -20,28 +33,37 @@ const server = http.createServer((req, res) => {
             contentType = 'text/css';
             break;
     }
+    sendFile(res, filePath, contentType);
+}
 
-    fs.readFile(filePath, (err, content) => {
-        if (err) {
-            if (err.code === 'ENOENT') {
-                // Arquivo não encontrado
-                res.writeHead(404, { 'Content-Type': 'text/html' });
-                res.end('<h1>404 Not Found</h1>');
-            } else {
-                // Erro do servidor
-                res.writeHead(500, { 'Content-Type': 'text/html' });
-                res.end(`<h1>500 Server Error</h1><p>${err}</p>`);
-            }
-        } else {
-            // Sucesso
-            res.writeHead(200, { 'Content-Type': contentType });
-            res.end(content, 'utf-8');
+const format = (input) => {
+    const result = []
+    for (const line of input.split('\n')) {
+        const [date, ticker, exchange, open, high, low, close, adjClose, volume] = line.split(',')
+        const data = {
+            date, ticker, exchange, open, high, low, close, adjClose, volume
         }
-    });
+        result.push(data)
+    }
+    return JSON.stringify(result);
+}
+
+const server = http.createServer((req, res) => {
+    if (req.url.includes('/api/location')) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET');
+        res.setHeader('Access-Control-Allow-Headers', 'content-type');
+        const url = new URL('http://localhost:3000' + req.url);
+        const location = url.searchParams.get('search');
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        const content = readFileSync("public/100.txt").toString();
+        return res.end(format(content))
+    }
+    
+    handleServeFiles(req, res);
 });
 
-const PORT = process.env.PORT || 3000;
-
+const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Servidor está rodando na porta ${PORT}`);
 });
